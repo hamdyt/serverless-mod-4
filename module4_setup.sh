c3cf74812ab1514d517f82e18c67bf1ffc3c4cce
hamdyt <eed738@hotmail.com>
2023-12-12 09:16:21 -0600

    Add files via upload

--- module4_setup.sh
+++ module4_setup.sh
@@ -1,11 +1,12 @@
-# This script deploys initial state of the Serverless Patterns workshop Module 4
+# This script deploys initial state of the Serverless Patterns workshop Module 3
 # Cleanup - delete module start state setup package
 rm -f ../ws-serverless-patterns.zip
 # Cleanup - delete module directory if it exists already
 rm -Rf ./userprofile
 # Get module start state setup package and unzip it into a temporary directory
 echo "Downloading module start state setup package..."
-wget -O ws-serverless-patterns.zip https://github.com/hamdyt/serverless-mod-4/raw/main/ws-serverless-patterns-2023-12-10.zip
+#wget -O ws-serverless-patterns.zip 'https://github.com/hamdyt/serverless-mod-4/raw/main/ws-serverless-patterns-2023-12-10.zip' -q
+wget -O ws-serverless-patterns.zip https://github.com/hamdyt/serverless-mod-4/blob/61fb02c11b7f9b76b880eca60915125675202e81/ws-serverless-patterns-2023-12-10.zip
 unzip -qq ws-serverless-patterns.zip 'ws-serverless-patterns/userprofile/*' -d ./tmp/
 # Move module directory with start state into workshop root directory
 echo "Setting up module directory..."
